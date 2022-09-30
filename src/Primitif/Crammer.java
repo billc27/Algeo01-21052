da6eb406b6d88f593b0eb17fa1ed9545c448181e
melvinkj <melvinkentj@gmail.com>
2022-09-30 18:48:23 +0700

    Create Crammer.java
    input is still in augmented Matrix. Need some adjustment to the specification

---
+++ src/Primitif/Crammer.java
@@ -0,0 +1,27 @@
+package Primitif;
+
+public class Crammer {
+    
+    // membaca matriks augmented
+    public static void splCrammer(Matrix m){
+        //I.S m berupa matriks augmented
+        Matrix m1;
+        int i;
+        double det, det1;
+
+        det = OBE.detOBE(m);
+        det1 = 1;
+        m1 = m.copyMatrix();
+
+        for(i=0; i<m.getCol()-1; i++){
+            m1.swapCol(i, m.getCol()-1);
+            det1 = OBE.detOBE(m1);
+            System.out.println(det1/det);
+            m1 = m.copyMatrix();
+        }
+    } 
+
+
+
+    
+}
