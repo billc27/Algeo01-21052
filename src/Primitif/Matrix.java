3f2bc45fedf6672643e019acc8e22340a0c84b10
billc27 <110593711+billc27@users.noreply.github.com>
2022-10-02 00:17:25 +0700

    Added inputManualReg and moved matRegression

--- src/Primitif/Matrix.java
+++ src/Primitif/Matrix.java
@@ -1,7 +1,7 @@
 package Primitif;
 import java.util.Scanner;
 
-/**
+/*
 13521052 Melvin Kent
 13521064 Bill Clinton
 13521100 Alexander Jason
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Matrix {
     int rows;
     int cols;
-    double[][] matrix;
+    public double[][] matrix;
     final double MARK = Double.NaN;
     Scanner scanElmt = new Scanner(System.in);
 
@@ -296,25 +296,10 @@ public class Matrix {
         return sigmaRow;
     }
     
-    public Matrix inputRegression() {
-        int var, titik;
-        Scanner scanreg = new Scanner(System.in);
-
-        // Input banyak variabel peubah
-        System.out.print("Masukkan jumlah variable peubah: ");
-        var = scanreg.nextInt();
-
-        // Input jumlah titik
-        System.out.print("Masukkan jumlah titik yang ingin dimasukkan: ");
-        titik = scanreg.nextInt();
-
-        // Isi matrix
-        Matrix mawal;
-        mawal = new Matrix(titik, var + 1);
-        double elmt;
+    public Matrix inputManualReg(Matrix mawal) {
         int i, j;
+        double elmt;
         Scanner scanElmt = new Scanner(System.in);
-        
         for (j = 0; j < mawal.getRow(); j++) {
             for (i = 0; i < mawal.getCol(); i++) {
                 if (i != mawal.getCol() - 1) {
@@ -329,28 +314,6 @@ public class Matrix {
                 }
             }
         }
-        mawal = mawal.Transpose();
-        Matrix mreg;
-        mreg = new Matrix(var + 1, var + 2);
-        
-        for (i = 0; i < mreg.getRow(); i++) {
-            for (j = 0; j < mreg.getCol(); j++) {
-                if (i == 0) {
-                    if (j == 0) {
-                        mreg.matrix[i][j] = titik;
-                    } else {
-                        mreg.matrix[i][j] = countRow(mawal, titik, i, j);
-                    }
-                } else {
-                    if (j == 0) {
-                        mreg.matrix[i][j] = mreg.matrix[j][i];
-                    } else {
-                        mreg.matrix[i][j] = countRow(mawal, titik, i, j);
-                    }
-                }
-
-            }
-        }
-        return mreg;
+        return mawal;
     }
 }
